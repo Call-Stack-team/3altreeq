642269a6d42aeed95824d8d9c594ee8b919c2081
ahmadalasaad <ahmadalserese9754@gnail.com>
2021-12-09 18:13:58 +0200

    first commit

---
+++ app/src/main/java/com/amplifyframework/datastore/generated/model/User.java
@@ -0,0 +1,191 @@
+package com.amplifyframework.datastore.generated.model;
+
+import com.amplifyframework.core.model.temporal.Temporal;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.Objects;
+
+import androidx.core.util.ObjectsCompat;
+
+import com.amplifyframework.core.model.Model;
+import com.amplifyframework.core.model.annotations.Index;
+import com.amplifyframework.core.model.annotations.ModelConfig;
+import com.amplifyframework.core.model.annotations.ModelField;
+import com.amplifyframework.core.model.query.predicate.QueryField;
+
+import static com.amplifyframework.core.model.query.predicate.QueryField.field;
+
+/** This is an auto generated class representing the User type in your schema. */
+@SuppressWarnings("all")
+@ModelConfig(pluralName = "Users")
+public final class User implements Model {
+  public static final QueryField ID = field("User", "id");
+  public static final QueryField FIRST_NAME = field("User", "firstName");
+  public static final QueryField LAST_NAME = field("User", "lastName");
+  private final @ModelField(targetType="ID", isRequired = true) String id;
+  private final @ModelField(targetType="String", isRequired = true) String firstName;
+  private final @ModelField(targetType="String") String lastName;
+  private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime createdAt;
+  private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime updatedAt;
+  public String getId() {
+      return id;
+  }
+  
+  public String getFirstName() {
+      return firstName;
+  }
+  
+  public String getLastName() {
+      return lastName;
+  }
+  
+  public Temporal.DateTime getCreatedAt() {
+      return createdAt;
+  }
+  
+  public Temporal.DateTime getUpdatedAt() {
+      return updatedAt;
+  }
+  
+  private User(String id, String firstName, String lastName) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  
+  @Override
+   public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      } else if(obj == null || getClass() != obj.getClass()) {
+        return false;
+      } else {
+      User user = (User) obj;
+      return ObjectsCompat.equals(getId(), user.getId()) &&
+              ObjectsCompat.equals(getFirstName(), user.getFirstName()) &&
+              ObjectsCompat.equals(getLastName(), user.getLastName()) &&
+              ObjectsCompat.equals(getCreatedAt(), user.getCreatedAt()) &&
+              ObjectsCompat.equals(getUpdatedAt(), user.getUpdatedAt());
+      }
+  }
+  
+  @Override
+   public int hashCode() {
+    return new StringBuilder()
+      .append(getId())
+      .append(getFirstName())
+      .append(getLastName())
+      .append(getCreatedAt())
+      .append(getUpdatedAt())
+      .toString()
+      .hashCode();
+  }
+  
+  @Override
+   public String toString() {
+    return new StringBuilder()
+      .append("User {")
+      .append("id=" + String.valueOf(getId()) + ", ")
+      .append("firstName=" + String.valueOf(getFirstName()) + ", ")
+      .append("lastName=" + String.valueOf(getLastName()) + ", ")
+      .append("createdAt=" + String.valueOf(getCreatedAt()) + ", ")
+      .append("updatedAt=" + String.valueOf(getUpdatedAt()))
+      .append("}")
+      .toString();
+  }
+  
+  public static FirstNameStep builder() {
+      return new Builder();
+  }
+  
+  /** 
+   * WARNING: This method should not be used to build an instance of this object for a CREATE mutation.
+   * This is a convenience method to return an instance of the object with only its ID populated
+   * to be used in the context of a parameter in a delete mutation or referencing a foreign key
+   * in a relationship.
+   * @param id the id of the existing item this instance will represent
+   * @return an instance of this model with only ID populated
+   */
+  public static User justId(String id) {
+    return new User(
+      id,
+      null,
+      null
+    );
+  }
+  
+  public CopyOfBuilder copyOfBuilder() {
+    return new CopyOfBuilder(id,
+      firstName,
+      lastName);
+  }
+  public interface FirstNameStep {
+    BuildStep firstName(String firstName);
+  }
+  
+
+  public interface BuildStep {
+    User build();
+    BuildStep id(String id);
+    BuildStep lastName(String lastName);
+  }
+  
+
+  public static class Builder implements FirstNameStep, BuildStep {
+    private String id;
+    private String firstName;
+    private String lastName;
+    @Override
+     public User build() {
+        String id = this.id != null ? this.id : UUID.randomUUID().toString();
+        
+        return new User(
+          id,
+          firstName,
+          lastName);
+    }
+    
+    @Override
+     public BuildStep firstName(String firstName) {
+        Objects.requireNonNull(firstName);
+        this.firstName = firstName;
+        return this;
+    }
+    
+    @Override
+     public BuildStep lastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+    
+    /** 
+     * @param id id
+     * @return Current Builder instance, for fluent method chaining
+     */
+    public BuildStep id(String id) {
+        this.id = id;
+        return this;
+    }
+  }
+  
+
+  public final class CopyOfBuilder extends Builder {
+    private CopyOfBuilder(String id, String firstName, String lastName) {
+      super.id(id);
+      super.firstName(firstName)
+        .lastName(lastName);
+    }
+    
+    @Override
+     public CopyOfBuilder firstName(String firstName) {
+      return (CopyOfBuilder) super.firstName(firstName);
+    }
+    
+    @Override
+     public CopyOfBuilder lastName(String lastName) {
+      return (CopyOfBuilder) super.lastName(lastName);
+    }
+  }
+  
+}
