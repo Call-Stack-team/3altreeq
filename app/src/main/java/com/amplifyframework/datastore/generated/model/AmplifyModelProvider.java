d50c255c83697976580ba5cf0a670bf82200c71f
ahmadalasaad <ahmadalserese9754@gnail.com>
2021-12-11 19:46:11 +0200

    model ride and user

--- app/src/main/java/com/amplifyframework/datastore/generated/model/AmplifyModelProvider.java
+++ app/src/main/java/com/amplifyframework/datastore/generated/model/AmplifyModelProvider.java
@@ -13,7 +13,7 @@ import java.util.Set;
  */
 
 public final class AmplifyModelProvider implements ModelProvider {
-  private static final String AMPLIFY_MODEL_VERSION = "db8d29e8bdb9dc0b083e3a03f4b977ed";
+  private static final String AMPLIFY_MODEL_VERSION = "3080a10c0ee89f8ae476c6c1aaded8df";
   private static AmplifyModelProvider amplifyGeneratedModelInstance;
   private AmplifyModelProvider() {
     
@@ -34,7 +34,7 @@ public final class AmplifyModelProvider implements ModelProvider {
   @Override
    public Set<Class<? extends Model>> models() {
     final Set<Class<? extends Model>> modifiableSet = new HashSet<>(
-          Arrays.<Class<? extends Model>>asList(User.class)
+          Arrays.<Class<? extends Model>>asList(Ride.class, RideUser.class, User.class)
         );
     
         return Immutable.of(modifiableSet);
